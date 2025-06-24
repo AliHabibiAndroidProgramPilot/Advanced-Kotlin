e43d7f11c237d1fd8a06713739285d7ae33255b6
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-06-24 16:40:32 +0330

    Decimal Formatting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,6 @@
+import java.text.DecimalFormat
+
 fun main() {
-    // implement a range to a list
-    val rangeList = (0..10).toList()
-    println(rangeList)
-
-    // spilt a list with a delimiters
-    val splitList = "Ali -- Alireza -- Mohammad -- Nazi -- Omid -- Ayda".split("--")
-    println(splitList)
-
-    // check multiple contains of a list
-    val mainList = listOf<Int>(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
-    println(mainList.containsAll(listOf(40, 50, 60, 88)))
-
-    // pull out a sub list from a list
-    val subList = listOf<Int>(10, 11, 12, 13, 14, 15).subList(1, 4)
-    println(subList)
-
-    val reversed = listOf<Int>(10, 20, 30, 40, 50, 60, 70, 80, 90, 100).asReversed()
-    println(reversed)
+    val earthPeople = 8_000_000_000L
+    println(DecimalFormat("#,###").format(earthPeople))
 }
