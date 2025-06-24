2e9fbe4e583b6af01ca34acbd5e64f975cb5f919
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-06-24 22:52:26 +0330

    Lambda Function With Specified Output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 fun main() {
-    val average = { number1: Int, number2: Int ->
+    val average: (number1: Int, number2: Int) -> Int = { number1, number2 ->
         (number1 + number2) / 2
     }
     println(average(10, 20))
+
+    val sayHello: (name: String) -> String = { name ->
+        "Hello, Welcome $name"
+    }
+    println(sayHello("Ali"))
 }
