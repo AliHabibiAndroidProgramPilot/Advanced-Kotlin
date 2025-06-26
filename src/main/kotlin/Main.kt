d6c7cbd4a18f3dea9862331986fc2c8ebdeec4c2
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-06-26 21:18:34 +0330

    Practical Lambda Usage Example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
+import java.net.InetAddress
+
 fun main() {
-    val message = "Hello to Kotlin"
-    showAlert(message) {
-        println("test")
-    }
+    println(
+        connectionManager("www.google.com")
+    )
 }
 
-private fun showAlert(text: String, listener: (text: String) -> Unit) {
-    listener(text)
+private val connectionManager: (address: String) -> Boolean = { address ->
+    try {
+        InetAddress.getByName(address)
+            .isReachable(3000)
+    } catch (e: Exception) {
+        println(e.stackTraceToString())
+        false
+    }
 }
